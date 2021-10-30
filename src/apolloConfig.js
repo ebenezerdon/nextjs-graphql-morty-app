c8624daad67e1c134e62d7bf81d32e76b4ad047a
ebenezerdon <ebenezerdonu@gmail.com>
2021-10-30 23:12:25 +0100

    use string instead of env variable

--- src/apolloConfig.js
+++ src/apolloConfig.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL,
+  uri: 'https://rickandmortyapi.com/graphql',
   cache: new InMemoryCache()
 })
 
